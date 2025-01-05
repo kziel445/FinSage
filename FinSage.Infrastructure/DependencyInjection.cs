e42f28fce7e0fbb6cbfa508aaf755f756ab680f8
Kamil Zieliński <47740876+kziel445@users.noreply.github.com>
2025-01-05 12:23:45 +0100

    create db configuration
    -update-database creates empty db

--- FinSage.Infrastructure/DependencyInjection.cs
+++ FinSage.Infrastructure/DependencyInjection.cs
@@ -1,17 +1,23 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using FinSage.Infrastructure.Persistence;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FinSage.Infrastructure
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services,
+            IConfiguration configuration)
         {
+            services.AddScoped<ApplicationDbContext>();
+
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<ApplicationDbContext>(options => 
+            options.UseSqlServer(connectionString).
+            EnableSensitiveDataLogging());
+
             return services;
         }
 
---
+++ FinSage.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinSage.Infrastructure.Persistence
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}

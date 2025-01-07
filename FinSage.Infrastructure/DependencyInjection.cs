477e8fa745ded39b1ed5baa66404ed8809a17407
Kamil Zieliński <47740876+kziel445@users.noreply.github.com>
2025-01-07 18:26:52 +0100

    Basic structure (#1)
    * add WebAPI project
    
    * add basic projects
    
    * add db commands
    
    * add connection string
    
    * add dependency injection
    
    * create db configuration
    
    -update-database creates empty db

---
+++ FinSage.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,29 @@
+﻿using FinSage.Infrastructure.Persistence;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FinSage.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddScoped<ApplicationDbContext>();
+
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<ApplicationDbContext>(options => 
+            options.UseSqlServer(connectionString).
+            EnableSensitiveDataLogging());
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseInfrastructure(this IApplicationBuilder app)
+        {
+            return app;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # FinSage
+
+# 1 Data base
+### Structure
+- Domain - model
+- Application - interface with DbSets
+- Infrastructure - configurations of model, ApplicationDbContext, Extensions
+(include starting account "admin")
+
+- !Unsecure!: 
+    1. "DefaultConnection": "Data Source=.;Initial Catalog=FinSage;User Id=kamil1;Password=1"
+    2. options.UseSqlServer(connectionString).
+            EnableSensitiveDataLogging()
+
+### Create from scratch
+- PM: 
+update-database
+- CMD from main project location  
+cd */FinSage  
+dotnet ef database update --project FinSage.Infrastructure --startup-project FinSage.WebAPI
+
+### Create Migration
+- PM:  
+Add-Migration [name] -outputdir [Path]
+- CMD:  
+dotnet ef migrations add [name] --output-dir Persistence/Migrations --project FinSage.Infrastructure --startup-project FinSage.WebAPI
